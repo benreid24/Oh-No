58032061f06eacc1af6e233009066a902c1aada7
Chriszosh96 <czosh1@binghamton.edu>
2021-09-12 00:44:30 -0400

    outsourcing to controller

--- entity/entity.py
+++ entity/entity.py
@@ -1,5 +1,6 @@
 from camera import Camera
 import math
+from main.py
 
 class Entity:
     def __init__(self, position, angle = 180, graphic):
@@ -10,20 +11,12 @@ class Entity:
         self.velocity_y = 0
         self.accel_x = 0
         self.accel_y = 0
-        self.accel = False
-        self.rotate_clockwise = False
-        self.rotate_counterclockwise = False
-
+        
     def update(self, dt):
         pass
     
     def update_physics(self, rotate_rate, accel_rate, dt):
-        self.accel_x = accel_rate * dt * cos_angle(self.angle)
-        self.accel_y = accel_rate * dt * sin_angle(self.angle)
-        self._rotate(rotate_rate, dt)
-        self._position(dt)
-        self._accelerate(dt, self.accel_x , self.accel_y)
-        pass
+        self._position(self, dt)
 
     def render(self, screen, camera: Camera):
         rect = self.graphic.get_rect(
@@ -36,16 +29,14 @@ class Entity:
 
     def _position(self, dt):
         pos_x += self.velocity_x * dt + .5 * self.accel_x * dt**2
-        pos_y = self.velocity_y * dt + .5 * self.accel_y * dt**2
+        pos_y += self.velocity_y * dt + .5 * self.accel_y * dt**2
         self.position = (pos_x ,pos_y )
 
     def _accelerate(self, dt):
-        if self.accel == True:
-            self.velocity_x += self.accel_x
-            self.velocity_y += self.accel_y
+        self.accel_x = accel_rate * dt * cos_angle(self.angle)
+        self.accel_y = accel_rate * dt * sin_angle(self.angle)
+        self.velocity_x += self.accel_x
+        self.velocity_y += self.accel_y
 
     def _rotate(self, rotate_rate, dt):
-        if self.rotate_clockwise == True:
-            self.angle = (self.angle + rotate_rate * dt ) % 360
-        if self.rotate_counterclockwise == True:
-            self.angle = (self.angle - rotate_rate * dt ) % 360
+        self.angle = (self.angle + rotate_rate * dt ) % 360
