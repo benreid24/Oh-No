1edf856ec52907bc63a329d3e7c6c245280bbeb5
Chriszosh96 <czosh1@binghamton.edu>
2021-10-15 01:05:07 -0400

    Added circle_graphics

---
+++ component/circle_graphic.py
@@ -0,0 +1,25 @@
+from typing import Tuple
+
+import pygame
+
+from .graphics import Graphics
+from camera import Camera
+from entity.entity import Entity
+
+
+class CircleGraphic(Graphics):
+    """
+    Basic circle graphic
+    """
+
+    def __init__(self, color, radius):
+        # type: (Tuple[int,int,int], int) -> None
+
+        super().__init__()
+        self.color = color
+        self.radius = radius
+
+    def render(self, screen, camera, owner):
+        # type: (any, Camera, Entity) -> None
+
+        pygame.draw.circle(screen, self.color, camera.transform(owner.position), self.radius)
