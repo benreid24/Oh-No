273783a6eb75307bf38c7bfc5e5c3904e8701f34
Ben Reid <reidben24@gmail.com>
2021-10-14 23:11:22 -0400

    Clean up Collidable component

--- component/collidable.py
+++ component/collidable.py
@@ -5,14 +5,13 @@ from component.position import Vector
 from .component import Component
 from entity.entity import Entity
 
-
-class BoundingArea:
+class Collidable(Component):
     class BoundType(Enum):
         Box = 1
         Circle = 2
 
     def __init__(self, type, mass=1, width=0, height=0, radius=0):
-        # type: (BoundingArea.BoundType, float, float, float, float) -> None
+        # type: (Collidable.BoundType, float, float, float, float) -> None
 
         self.center = Vector(0, 0)
         self.type = type
@@ -21,11 +20,15 @@ class BoundingArea:
         self.height = height
         self.radius = radius
 
+    def update(self, dt, owner):
+        # type: (float, Entity) -> None
+        self.center = owner.position
+
     def is_colliding(self, other):
-        # type: (BoundingArea) -> bool
+        # type: (Collidable) -> bool
         
         def box_overlap(l, r):
-            # type: (BoundingArea, BoundingArea) -> bool
+            # type: (Collidable, Collidable) -> bool
             # TODO - this assumes that the rects are aligned. We need to axis align them first
             dx = l.center.x - r.center.x
             dy = l.center.y - r.center.y
@@ -34,39 +37,25 @@ class BoundingArea:
             return abs(dx) < hws and abs(dy) < hhs
 
         def circle_overlap(l, r):
-            # type: (BoundingArea, BoundingArea) -> bool
+            # type: (Collidable, Collidable) -> bool
             dx = l.center.x - r.center.x
             dy = l.center.y - r.center.y
             rs = l.radius + r.radius
             return dx*dx + dy*dy < rs*rs
 
         def box_circle_overlap(box, circle):
-            # type: (BoundingArea, BoundingArea) -> bool
+            # type: (Collidable, Collidable) -> bool
             # TODO - this is actually pretty hard
             pass
 
-        if self.type == BoundingArea.BoundType.Box:
-            if other.type == BoundingArea.BoundType.Box:
+        if self.type == Collidable.BoundType.Box:
+            if other.type == Collidable.BoundType.Box:
                 return box_overlap(self, other)
             return box_circle_overlap(self, other)
-        if other.type == BoundingArea.BoundType.Box:
+        if other.type == Collidable.BoundType.Box:
             return box_circle_overlap(other, self)
         return circle_overlap(self, other)
 
     def get_collision_angle(self, other):
-        # type: (BoundingArea) -> float
+        # type: (Collidable) -> float
         return math.atan2(self.center.y - other.center.y, self.center.x - other.center.x) * 180 / math.pi
-
-
-class Collidable(Component):
-    def __init__(self, bounds):
-        # type: (BoundingArea) -> None
-        self.bounds = bounds
-
-    def update(self, dt, owner):
-        # type: (float, Entity) -> None
-        self.bounds.center = owner.position
-
-    def is_colliding(self, other):
-        # type: (Collidable) -> bool
-        return self.bounds.is_colliding(other.bounds)
